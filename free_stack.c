cb2b1e355d20759912a5adaf97a540b7c4c9a583
Mamushie <wawerumushie@gmail.com>
2021-12-02 23:59:23 +0300

    Add new file

---
+++ free_stack.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+/**
+ * free_stack - frees a doubly linked list
+ * @head: a pointer to the first node in the list
+ * Return: No Value
+ */
+void free_stack(stack_t *head)
+{
+	if (head == NULL)
+		return;
+	for (; head->next; head = head->next)
+	{
+		if (head->prev)
+			free(head->prev);
+	}
+	free(head->prev);
+	free(head);
+}
